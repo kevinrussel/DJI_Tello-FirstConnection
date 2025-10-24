b5713200401f0272d777b08e9229d91308ffb0ba
kevin russel <kzone159@gmail.com>
2025-10-23 22:48:15 -0500

    finished connecting and flying drone

--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
-from time import sleep
 from djitellopy import Tello
-
-t = Tello(retry_count=1)
-t.LOGGER.setLevel("DEBUG")
-t.connect()                 # puts drone in SDK 'command' mode
-sleep(1.0)                  # give sensors a beat
-print("Battery:", t.get_battery())
-print("Temp:", t.get_temperature())
-print("Height:", t.get_height())
-t.takeoff()
-t.send_rc_control(0,0,0,0)
-t.land()
-t.end()
+tello = Tello()
+tello.connect()
+tello.takeoff()
+tello.land()
+tello.end()
